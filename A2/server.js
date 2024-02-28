830634c963a13c443b8ae0f79700b0bc8eb5ea76
Mike Thai <mikethai126@gmail.com>
2024-02-28 00:11:55 -0800

    Add files via upload

---
+++ A2/server.js
@@ -0,0 +1,27 @@
+let express = require('express');
+let fs = require('fs');
+
+app = express();
+
+function copyFile(copy) {
+    fs.readFile("original.txt", "utf8", function(err,data) {
+        
+        for (let i = 0; i < copy; i++) {
+            fs.writeFileSync("copy_" + (i+1), data);
+            console.log(i);
+        }
+        
+        console.log("Done copying " + copy + " files!");
+    })
+}
+
+//copyFile(6);
+
+app.get('/:number', function(req, resp){
+    let copy = parseInt(req.params.number);
+    copyFile(copy);
+    resp.send("Done copying " + copy + " files!");
+});
+
+
+app.listen(3000);
